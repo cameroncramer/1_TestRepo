9845fd24c911eaec8b0856908a27b23313b20ac6
Cameron Cramer <cam@miprofs.com>
2015-01-09 09:59:09 -0800

    2nd R Programming Assignment
    This is the submitted work for the second R Programming Assignment

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,24 @@
+makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+  set<-function(y){							#Sets the value of the matrix
+  x<<-y										#Assigns y to x in the parent env
+  m<<-NULL									#Assigns NULL to M in the parent env
+}
+get<-function() x							#Gets the value of the matrix
+setmatrix<-function(solve) m<<- solve		#Sets the value of the inverse matrix
+getmatrix<-function() m						#Gets the value of the inverse matrix	
+list(set=set, get=get,
+   setmatrix=setmatrix,
+   getmatrix=getmatrix)
+}
+
+cacheSolve <- function(x=matrix(), ...) {
+    m<-x$getmatrix()						#Checks if matrix inverse already calculated
+    if(!is.null(m)){
+      message("getting cached data")
+      return(m)
+    }
+    matrix<-x$get()							#Calculates matrix inverse if not done already 
+    m<-solve(matrix, ...)					#Defines m as the matrix inverse
+    x$setmatrix(m)
+    m										#Returns the matrix inverse
